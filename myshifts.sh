ce6717e0e1b21d9416370d8c12b85815bd77539b
Tiago Donato <tgdnt@pm.me>
2018-12-12 22:05:56 -0600

    Removes date format conversions from script
    * Changes defaults in .myshifts.config.sample to %Y-%m-%d
    * Removes date conversion from `khal new` and from `khal list`
    * Now the user has to input in the correct dateformat for khal

--- myshifts.sh
+++ myshifts.sh
@@ -8,7 +8,7 @@ days=0
 
 # Loop through args and create events
 while [ $1 ] ; do # for as many times you add a timestamp
-	[ $1 != "off" ] && $khal new -a $calendar $(date -j -v+"$days"d -f "%m/%d" "+$dateformat" $startdate) $1 $shiftlength $eventname
+	[ $1 != "off" ] && $khal new -a $calendar $(date -j -v+"$days"d -f "$dateformat" "+$dateformat" $startdate) $1 $shiftlength $eventname
 	let days++
 	shift # get next day's start time
 done
@@ -16,6 +16,6 @@ done
 [ $sync == 1 ] && $vdirsyncer sync # If sync is on, perform sync
 
 # Print days
-$khal list $(date -j -f "%m/%d" "+$dateformat" $startdate) "$days"d
+$khal list $startdate "$days"d
 
 exit 0
